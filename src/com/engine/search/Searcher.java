0f64d1d007ddc7e5a9b730cd6b4dc9fc0df0a495
Alexis <alexis.king@lifars.com>
2016-04-18 15:41:52 -0400

    started searcher class, do not merge

---
+++ src/com/engine/search/Searcher.java
@@ -0,0 +1,16 @@
+package com.engine.search;
+
+import java.util.ArrayList;
+import com.engine.mediator.data.*;
+/**
+ * Searcher contains methods for searching for cars in the database
+ * @author Alexis King
+ */
+public class Searcher {
+	
+	public ArrayList<Car> searchCar(String make, String model, String year){
+		ArrayList<Car> cars = new ArrayList<Car>();
+		
+		return cars;
+	}
+}
